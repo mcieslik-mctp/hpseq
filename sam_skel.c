da2da50dd7dabf7cb0713e40d6e964fa647bed6e
Marcin Cieslik <mcieslik@med.umich.edu>
2015-01-23 23:55:13 +0000

    fixes

--- sam_skel.c
+++ sam_skel.c
@@ -1,13 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <hts.h>
+#include <sam.h>
 
-int main() {
 
-  htsFile* hfi = hts_open("test/pe_alig.bam", "r");
-  const htsFormat* z;
-  z = hts_get_format(hfi);
-  printf("%d\n", z->format);
-  
+int main(int argc, char *argv[]) {
+  const htsFormat* fmt;
+  bam_hdr_t *hdr;
+  htsFile* inp;
+  /* htsFile* out; */
+  inp = hts_open(argv[1], "r");
+  /* out = hts_open("-", "w"); */
+
+  fmt = hts_get_format(inp);
+  printf("%d\n", fmt->format);
+   
+  /*  sam_index_load */
+  /* sam_hdr_write(out, h); */
   return 0;
 }
