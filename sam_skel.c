94c13b4d32ecfc127d6afa5e6c03aca672d8e8fb
Marcin Cieslik <mcieslik@med.umich.edu>
2015-01-25 05:08:44 +0000

    bed and sam output

--- sam_skel.c
+++ sam_skel.c
@@ -2,39 +2,56 @@
 #include <stdio.h>
 #include <hts.h>
 #include <sam.h>
+#include <string.h>
 
 
 int main(int argc, char *argv[]) {
   int ret;
   int exit_code = 0;
-  const htsFormat* fmt;
   bam_hdr_t *hdr;
-  htsFile* inp;
-  htsFile* out;
-  inp = hts_open(argv[1], "r");
-  out = hts_open("-", "w");
-
-  fmt = hts_get_format(inp);
-  printf("%d\n", fmt->format);
+  htsFile *inp;
+  htsFile *out_sam;
+  FILE *out_bed;
+  const htsFormat *fmt;
   
+  inp = hts_open(argv[1], "r");
   hdr = sam_hdr_read(inp);
-  sam_hdr_write(out, hdr);
+  fmt = hts_get_format(inp);
+  fprintf(stderr, "format: %d\n", fmt->format);
 
-  //
-  bam1_t *b;
-  b = bam_init1();
-  while ((ret = sam_read1(inp, hdr, b)) >= 0) {
-    if (sam_write1(out, hdr, b) < 0) {
+  
+  // change extension of output file
+  strcpy(strrchr(argv[1], '.'), ".sam");
+  out_sam = hts_open(argv[1], "w");
+  sam_hdr_write(out_sam, hdr);
+
+  // change extension of output file
+  strcpy(strrchr(argv[1], '.'), ".bed");
+  // 
+  out_bed = fopen(argv[1], "wb");
+  bam1_t *br;
+  br = bam_init1();
+  while ((ret = sam_read1(inp, hdr, br)) >= 0) {
+    fprintf(out_bed, "%s\t%d\t%d\t%c\n", hdr->target_name[br->core.tid], br->core.pos,
+	    bam_endpos(br), bam_is_rev(br) ? '+' : '-');
+  }
+  bam_destroy1(br);
+  fclose(out_bed);
+
+  br = bam_init1();
+  rewind(inp);
+  while ((ret = sam_read1(inp, hdr, br)) >= 0) {
+    if (sam_write1(out_sam, hdr, br) < 0) {
       fprintf(stderr, "Error writing output.\n");
       exit_code = 1;
       break;
     }
   }
   // clean-up
-  bam_destroy1(b);
+  bam_destroy1(br);
   bam_hdr_destroy(hdr);
   
-  ret = sam_close(out);
+  ret = sam_close(out_sam);
   if (ret < 0) {
     fprintf(stderr, "Error closing output.\n");
     exit_code = 1;
