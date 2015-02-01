790d2bc0ec9b2c4a0878e6aa9a8c5af80f3669de
Marcin Cieslik <mcieslik@med.umich.edu>
2015-02-01 20:26:13 +0000

    added reverse complement

--- hpmerge.c
+++ hpmerge.c
@@ -4,6 +4,8 @@
 #include <zlib.h>
 #include <kseq.h>
 
+#define MAX_READ_LENGTH 10000
+
 KSEQ_INIT(gzFile, gzread)
 
 
@@ -13,11 +15,15 @@ int main(int argc, char *argv[]) {
   int status = 0;
   char *fo = NULL;
   int drop = 0;
+  int revc = 1;
   
-  while ((c = getopt(argc, argv, "o:d")) >= 0) {
+  while ((c = getopt(argc, argv, "o:nd")) >= 0) {
     if (c == 'o') {
       fo = malloc((strlen(optarg) + 1));
       if (fo) strcpy(fo, optarg);
+    }
+    if (c == 'n') {
+      revc = 0;
     }
     if (c == 'd') {
       drop = 1;
@@ -30,6 +36,9 @@ int main(int argc, char *argv[]) {
   	"Usage: hpmerge -o <output.fq> <input_1.fq> <input_2.fq>\n\n");
   	fprintf(stderr, "Options:\n");
   	fprintf(stderr, "    -o STR    output file name\n");
+  	fprintf(stderr, "    -o STR    output file name\n");
+  	fprintf(stderr, "    -d        drop quality scores\n");
+  	fprintf(stderr, "    -n        do not reverse complement\n");
   	fprintf(stderr, "\n");
   	status = 1;
   } else {
@@ -44,9 +53,12 @@ int main(int argc, char *argv[]) {
   }
   
   if (!status) {
+    int last;
     FILE *fw;
     gzFile  fp[2];
     kseq_t *ks[2];
+    char *fs; // forward sequence
+    char rc[MAX_READ_LENGTH] = {0}; // reverse-complement
     
     fw = fopen(fo, "wb");
     fp[0] = gzopen(argv[optind], "rb");
@@ -58,7 +70,23 @@ int main(int argc, char *argv[]) {
       fputs(ks[0]->name.s, fw);
       fputc('\n', fw);
       fputs(ks[0]->seq.s, fw);
-      fputs(ks[1]->seq.s, fw);
+      // the second read is reverse-complemented
+      if (revc) {
+	last = ks[1]->seq.l-1;
+	fs = ks[1]->seq.s;
+	for (i=0; i<=last; ++i) {
+	  switch(fs[i]) {
+	  case 'A': rc[last - i]='T'; break;
+	  case 'T': rc[last - i]='A'; break;
+	  case 'G': rc[last - i]='C'; break;
+	  case 'C': rc[last - i]='G'; break;
+	  default:  rc[last - i]='N'; break;
+	  }
+	}
+	fputs(rc, fw);
+      } else {
+	fputs(ks[1]->seq.s, fw);
+      }
       fputc('\n', fw);
       if ((!drop) && ks[0]->qual.l) {
 	fputs("+", fw);
